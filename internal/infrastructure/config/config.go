5d05677a355490444f73d572b45c8277e3dc84da
rioooranteai <rioooranteai@gmail.com>
2026-08-16 05:58:15 +0700

    feat[auth]: add config with fully env data

--- internal/infrastructure/config/config.go
+++ internal/infrastructure/config/config.go
@@ -7,15 +7,21 @@ import (
 )
 
 type Config struct {
-	AppPort   string
-	DBPath    string
-	JWTSecret string
+	AppPort    string
+	DBPath     string
+	JWTSecret  string
+	AIProvider string
+	AIAPIKey   string
 }
 
 func Load() *Config {
 	godotenv.Load()
 
 	return &Config{
-		AppPort: os.Getenv("APP_PORT"),
+		AppPort:    os.Getenv("APP_PORT"),
+		DBPath:     os.Getenv("DB_PATH"),
+		JWTSecret:  os.Getenv("JWT_SECRET"),
+		AIProvider: os.Getenv("AI_PROVIDER"),
+		AIAPIKey:   os.Getenv("AI_API_KEY"),
 	}
 }
